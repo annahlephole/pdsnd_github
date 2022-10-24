03e54bbbda93d667e618ba7691c35924db16c49e
Litseoane <annah.lephole06@gmail.com>
2022-10-24 01:13:33 -0700

    refactor: update print statement to invalid option in get_filters function

--- bikeshare.py
+++ bikeshare.py
@@ -15,7 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data for three biggest cities!')
     print('-'*40)
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
@@ -24,7 +24,7 @@ def get_filters():
         if city in city_names:
             break
         else:
-            print('\nPlease enter name of city from named cities above: ')
+            print('\ninvalid input: ')
 
     # TO DO: get user input for month (all, january, february, ... , june)
     while True:
